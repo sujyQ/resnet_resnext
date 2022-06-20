aebf9939a2418df2fc4dd103d82b721ec1d4c3c2
sujyQ <tn0502wl@ajou.ac.kr>
2022-06-20 21:47:45 +0900

    upload train codes
    upload train resnet/resnext code

---
+++ train_resnet.py
@@ -0,0 +1,109 @@
+# code source1: https://www.kaggle.com/code/tiiktak/fashion-mnist-with-alexnet-in-pytorch-92-accuracy/notebook
+# code source2: https://pytorch.org/tutorials/
+import os
+import torch
+import torchvision
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from PIL import Image
+from torchvision import transforms, datasets
+from torch.utils.data import Dataset, DataLoader
+import torchvision.models as models
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '2'
+
+# training batches of our network
+EPOCHS = 50
+# size of each batch
+BATCH_SIZE = 48
+
+DEVICE = ("cuda" if torch.cuda.is_available() else "cpu")
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(torch.__version__)
+print(DEVICE)
+
+from torchvision.transforms import ToTensor
+
+transform=transforms.Compose([transforms.Resize((224, 224)),ToTensor(), transforms.Normalize( (0.5,), (0.5,))])
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=transform
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=transform
+)
+
+train_loader = DataLoader(training_data, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
+
+class MnistResNet(nn.Module):
+  def __init__(self, in_channels=1):
+    super(MnistResNet, self).__init__()
+
+    # Load a pretrained resnet model from torchvision.models in Pytorch
+    self.model = models.resnet50(pretrained=False)
+
+    self.model.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    num_ftrs = self.model.fc.in_features
+    self.model.fc = nn.Linear(num_ftrs, 10)
+
+  def forward(self, x):
+    return self.model(x)
+
+
+model = MnistResNet().to(DEVICE)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        target = target.type(torch.LongTensor)
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if (batch_idx + 1) % 50 == 0:
+            print("ResNet | Train Epoch:{} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)  # 
+        print("\nResNet | Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+        print('='*50)
+
+
+for epoch in range(1, EPOCHS+1):
+    train(model, DEVICE, train_loader, optimizer, epoch)
+    test(model, DEVICE, test_loader)
+    torch.save(model.state_dict(), "ResNet50.pth")
